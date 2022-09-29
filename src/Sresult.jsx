f53a145121336047a6aa6abf36574631feb10b61
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-09-29 19:15:00 +0530

    complete live search

--- src/Sresult.jsx
+++ src/Sresult.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Sresult(props) {
-    // const imgs = `https://source.unsplash.com/user/${props.images}/400x200/`;
+    // <img src="https://source.unsplash.com/user/erondu/1600x900"></img>
     const imgs = `https://source.unsplash.com/random/400x200/?${props.images}`;
   return (
     <>
