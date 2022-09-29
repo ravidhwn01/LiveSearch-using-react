4f4e6e3cf55ca233718d7cb206c95e916d6bc27d
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-09-29 17:51:09 +0530

    set up initial files

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Error from './Error'
+import User from "./User";
+import Search from "./Search";
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/user/ravi/dhawan">User</Link>
+          </li>
+          <li>
+            <Link to="/Search">Search</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+       
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/Contact">
+            <Contact />
+          </Route>
+          <Route path="/Search">
+            <Search />
+          </Route>
+          <Route path="/user/:fname/:lname">
+            <User />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+
+
---
+++ src/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div>
+        <input type="search" name="type here....." id="" />
+    </div>
+  )
+}
